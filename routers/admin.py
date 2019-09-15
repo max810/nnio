ef95538e0bf45984169f67add7a0e2f8f688ffb0
max810 <maksym.bekuzarov@gmail.com>
2019-09-16 00:29:36 +0300

    Updated JSON generation for reading layer_schemas.

--- routers/admin.py
+++ routers/admin.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from sqlalchemy.orm import Session
 from starlette.requests import Request
 from fastapi import Depends, FastAPI, HTTPException, APIRouter
+from starlette.responses import JSONResponse, Response
 
 from DAL import db_models
 from routers.common import oauth2_scheme, templates
@@ -19,11 +20,14 @@ def get_admin_page(request: Request):
 
 @router.get("/layers_schemas")
 def get_layers_schemas(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    layer_schemas = {}
+    layer_schemas_strs = []
     for schema in db.query(db_models.LayerSchema):
-        layer_schemas[schema.layer_type] = json.loads(schema.layer_schema)
+        layer_schemas_strs.append("{}: {}".format(schema.layer_type, schema.layer_schema))
 
-    return layer_schemas
+    res_str = ",".join(layer_schemas_strs)
+    res_str = "{" + res_str + "}"
+
+    return Response(content=res_str, media_type='application/json')
 
 
 @router.post("/save_layers_schemas")
@@ -31,6 +35,7 @@ def post_save_layers_schemas(body: dict, token: str = Depends(oauth2_scheme), db
     db.query(db_models.LayerSchema).delete()
 
     for layer_type, layer_schema in body.items():
-        db.add(db_models.LayerSchema(layer_type, json.dumps(layer_schema)))
+        #
+        db.add(db_models.LayerSchema(layer_type, json.dumps(layer_schema, separators=(',', ':'))))
     db.commit()
     # json.dump(layer_schema, open('BLL/layer_schemas_experimental/{}.json'.format(layer_type), 'wt'))
